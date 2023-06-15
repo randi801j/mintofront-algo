e253a0ce3df1eba1ec5294ab083c22504219f35d
Randy Luc <113475637+randi801j@users.noreply.github.com>
2023-06-14 21:04:55 -0700

    Update toFront.js

--- min to fron/toFront.js
+++ min to fron/toFront.js
@@ -1,16 +1,16 @@
 function minToFront(arr) {
-    var min = arr[0];
+    var min = arr[0];// save first in arr
     var idx = 0;
-    for (var i = 1; i < arr.length; i++){
-        if (arr[i] < min) {
-            min = arr[i];
-            idx = i;
+    for (var i = 1; i < arr.length; i++){ // loop through arr length (5)
+        if (arr[i] < min) {// if (1<4)
+            min = arr[i];//  set 4 to 1
+            idx = i;// idx set to 1 
         }
     }
-    for (var x = idx; x > 0; x--){
-        var temp = arr[x];
-        arr[x] = arr[x-1];
-        arr[x-1] = temp;
+    for (var x = idx; x > 0; x--){// x = 4
+        var temp = arr[x];// temp is set to 4
+        arr[x] = arr[x-1];//3
+        arr[x-1] = temp;//2
     }
     return arr;
 }
